06f7219470b9c5116a806e143cc197741c39a161
odin wong <odinwong16@gmail.com>
2021-05-15 17:58:13 +1000

    worked out subsquare iteration

--- sudoku-solver.py
+++ sudoku-solver.py
@@ -1,36 +1,48 @@
 import argparse
+import math
 from itertools import product
 
 
 
 class Sudoku():
     
-    def __init__(self, initialState, size=9):
-        self.size = size
-        self.grid = [[[n for n in range(1,10)] for x in range(size)] for u in range(size)]
+    def __init__(self, initialState):
+        #at this stage only allow 9x9 sudoku squares
+        self.size = 9
+        self.grid = [[[n for n in range(1,10)] for x in range(self.size)] for u in range(self.size)]
 
-        #put input arguments into grid to initialize starting values
-        for ((x,y),n) in zip(product(range(size), repeat=2), range(size**2)):
+        #put input arguments into grid to initialize starting values,
+        #only takes size^2 values from the inital state list provided
+        #TODO check size/initialState is a valid size
+        for ((x,y),n) in zip(product(range(self.size), repeat=2), range(self.size**2)):
             if initialState[n] != 0:
                 self.grid[x][y] = [initialState[n]]
             print(self.grid[x][y])
 
+        #each square contains 'size' number of cells, 
+        # there are 'size' number of squares in the whole puzzle
+        self.subSidelength = math.isqrt(self.size)
         
 
     def solve(self):
         pass
 
     #recursively check all the constraints starting from cell (x,y)
-    def make_consistent(grid, x, y):
+    def make_consistent(self, grid, x, y):
         #check row and column
-        for (m,n) in product(range(size),[y]):
+        for (m,n) in product(range(self.size),[y]):
             pass
 
-        for (m,n) in product([x], range(9)):
+        for (m,n) in product([x], range(self.size)):
             pass
 
         #check square
-        for (m,n) in product(range()):
+        ##first caculate the starting index of the subsquare that (x,y) is in
+        ##and then iterate over the whole square
+        xStart = (x//self.subSidelength)*self.subSidelength
+        yStart = (y//self.subSidelength)*self.subSidelength
+        for (m,n) in product(range(xStart, xStart+self.subSidelength), range(yStart, yStart+self.subSidelength)):
+            pass
 
     def show(self):
         pass
@@ -45,7 +57,10 @@ def main():
 
     print(len(args.Input))
 
+    sudoku = Sudoku(args.Input)
+
     #main arc consisitency checking loop
+
 
 
 
