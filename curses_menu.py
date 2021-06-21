b78b329026debba2f1a7b43140559b5a81181373
odin wong <odinwong16@gmail.com>
2021-06-21 16:42:38 +1000

    basic curses menu working with callback functions

--- curses_menu.py
+++ curses_menu.py
@@ -1,11 +1,79 @@
-#basic python curses menu class
+# basic python curses menu class
+
+import curses
+import sys
+
+class MenuItem():
+
+    def __init__(self, text: str, id: int, on_activate):
+        self._on_activate = on_activate
+        self._id = id
+        self.text = text
+
+    def activate(self):
+        self._on_activate(self.text, self._id)
+
 
 class CursesMenu():
 
-    def __init__(self):
-        #list of menu items
+    def __init__(self, parent_screen, y: int, x: int, height: int, width: int, quit_item: bool = True):
+        # list of menu items
         self.menu_items = []
+        self.menuscreen = parent_screen.derwin(height, width, y, x)
 
-    def add_item(text: str, callback: function):
-        pass
+        if quit_item:
+            self.add_item("quit", lambda s,i : sys.exit())
+
+    def add_item(self, text: str, on_activate):
+        maxy, _ = self.menuscreen.getmaxyx()
+        if len(self.menu_items) >= maxy:
+            return False
+        self.menu_items.append(
+            MenuItem(text, len(self.menu_items), on_activate)
+        )
+        return True
     
+    def _quit(s: str, i: int):
+        quit()
+
+    def add_toggle_item(self, text: str, on_activate, defaultstate: bool = True):
+        pass
+
+    def start_menu(self):
+
+        # draw menu
+        for index, item in enumerate(self.menu_items):
+            maxy, _ = self.menuscreen.getmaxyx()
+
+            if index < maxy:
+                self.menuscreen.addstr(index, 0, item.text)
+
+        # reset cursor
+        self.menuscreen.move(0, 0)
+
+        # main menu control loop
+        index, _ = self.menuscreen.getyx()
+        self.menuscreen.keypad(1)
+        while True:
+            key = self.menuscreen.getch()
+            self.menuscreen.addstr(index, 0, self.menu_items[index].text)
+
+
+            if key == curses.KEY_UP:
+                if index != 0:
+                    index -= 1
+            elif key == curses.KEY_DOWN:
+                if index != (len(self.menu_items) - 1):
+                    index += 1
+            elif key == curses.KEY_ENTER or key == ord('\n') or key == ord('\r'):
+                self.menu_items[index].activate()
+
+            #highlight current item
+            self.menuscreen.addstr(
+                index,
+                0,
+                self.menu_items[index].text,
+                curses.A_REVERSE
+            )
+
+            self.menuscreen.refresh()
--- sudoku_curses.py
+++ sudoku_curses.py
@@ -1,102 +1,94 @@
 import curses
 import curses.ascii
 from sudoku_solver import Sudoku
+from curses_menu import CursesMenu
+
 
 def main(scr):
     scr = curses.initscr()
     scr.clear()
 
-    sudoku_box = scr.derwin(13,13, 1, 1)
+    sudoku_box = scr.derwin(13, 13, 1, 1)
     sudoku_box.box()
 
     sudoku = Sudoku()
 
-    #setup grid in the sudoku window
-    for x in range(1,12):
-        for y in range(1,12):
-            if y%4==0 and x%4==0:
-                sudoku_box.addch(y,x,ord('+'), curses.A_BOLD)
-            elif y%4 == 0:
-                sudoku_box.addch(y,x,ord('-'), curses.A_BOLD)
-            elif x%4 == 0:
-                sudoku_box.addch(y,x,ord('|'), curses.A_BOLD)
+    # setup grid in the sudoku window
+    for x in range(1, 12):
+        for y in range(1, 12):
+            if y % 4 == 0 and x % 4 == 0:
+                sudoku_box.addch(y, x, ord('+'), curses.A_BOLD)
+            elif y % 4 == 0:
+                sudoku_box.addch(y, x, ord('-'), curses.A_BOLD)
+            elif x % 4 == 0:
+                sudoku_box.addch(y, x, ord('|'), curses.A_BOLD)
+    sudoku_box.refresh()
 
-    #setup menu
-    sby,sbx = sudoku_box.getmaxyx()
-    
-    scr.addstr(sby+1, 1, "'q' - quit")
-    scr.addstr(sby+3, 1, "edit")
-    scr.addstr(sby+4, 1, "solve")
+    # setup menu
+    sby, sbx = sudoku_box.getmaxyx()
+    screenmaxy, screenmaxx = scr.getmaxyx()
 
-    #main control loop
-    key = None
-    while True:
-        key = scr.getch()
+    menu = CursesMenu(scr, sby + 1, 0, 10, 20)
 
-        #control menu
-        
-        menuindex = 0
-        if key == ord('e'):
-            edit_mode(sudoku_box, sudoku)
-        elif key == ord("q"):
-            quit()
-        elif key == curses.KEY_UP:
-            pass
-        elif key == curses.KEY_DOWN:
-            pass
+    menu.add_item("edit sudoku", lambda a, i : edit_mode(sudoku_box, sudoku))
+
+    # main control loop
+    menu.start_menu()
 
 
 def edit_mode(sudoku_box, sudoku):
-    sudoku_box.move(1,1)
+    sudoku_box.move(1, 1)
     sudoku_box.keypad(1)
 
     key = None
     while True:
         key = sudoku_box.getch()
-        cy,cx = sudoku_box.getyx()
+        cy, cx = sudoku_box.getyx()
 
         if key == curses.KEY_RIGHT:
             cx += 1
-            if on_border(sudoku_box,cy,cx):
+            if on_border(sudoku_box, cy, cx):
                 continue
-            elif not valid_position(cy,cx):
+            elif not valid_position(cy, cx):
                 cx += 1
         elif key == curses.KEY_LEFT:
             cx -= 1
-            if on_border(sudoku_box,cy,cx):
+            if on_border(sudoku_box, cy, cx):
                 continue
-            elif not valid_position(cy,cx):
+            elif not valid_position(cy, cx):
                 cx -= 1
         elif key == curses.KEY_UP:
             cy -= 1
-            if on_border(sudoku_box,cy,cx):
+            if on_border(sudoku_box, cy, cx):
                 continue
-            elif not valid_position(cy,cx):
+            elif not valid_position(cy, cx):
                 cy -= 1
         elif key == curses.KEY_DOWN:
             cy += 1
-            if on_border(sudoku_box,cy,cx):
+            if on_border(sudoku_box, cy, cx):
                 continue
-            elif not valid_position(cy,cx):
+            elif not valid_position(cy, cx):
                 cy += 1
         elif key == ord('q'):
-            quit()
+            return
         elif curses.ascii.isdigit(key) and key != ord('0'):
             sudoku_box.addch(key)
-            sudoku.edit_cell(cx,cy, int(key))
+            sudoku.edit_cell(cx, cy, int(key))
         elif key == curses.KEY_BACKSPACE or key == ord('0'):
             sudoku_box.addch(' ')
-            sudoku.edit_cell(cx,cy, 0)
+            sudoku.edit_cell(cx, cy, 0)
 
-        sudoku_box.move(cy,cx)
+        sudoku_box.move(cy, cx)
 
-def valid_position(y,x):
-    if x%4 == 0 or y%4==0:
+
+def valid_position(y, x):
+    if x % 4 == 0 or y % 4 == 0:
         return False
     return True
 
-def on_border(scr,y,x):
-    my,mx = scr.getmaxyx()
+
+def on_border(scr, y, x):
+    my, mx = scr.getmaxyx()
 
     if y == 0 or x == 0 or x >= mx - 1 or y >= my - 1:
         return True
